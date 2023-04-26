d38d39908d708c13334cd43195411aa7feef07c1
stefanselic <s.stefan22@hotmail.com>
2023-04-26 23:07:13 +0200

    The random-color-generator generates now also specific colors based on the users input

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 <!-- @format -->
 
 # Node.js Random Color Generator
+
+<!--
+###############################
+###############################
+###############################
+#####                     #####
+#####        ff0000       #####
+#####                     #####
+###############################
+###############################
+###############################
+-->
+
+## TODO
+
+- [x] Create a Github repo
+- [x] Clone the Github repo
+- [x] Set up 'package.json'
+- [x] Set up ESLint config
+- [x] Create 'index.js' and implement the following:
+  - [x] Generate a random color (hex code)
+  - [x] Create the 31x9 hash pattern
+  - [x] Paint the pattern with the color
+  - [x] Accept input from the user for the hue
+  - [x] Accept input from the user for the luminosity
+  - [x] Adjust the random color based on the variables
--- index.js
+++ index.js
@@ -1,26 +1,45 @@
 /** @format */
-
+// npm chalk is installed | imported from chalk with import chalk from 'chalk';
 import chalk from 'chalk';
+import randomColor from 'randomcolor';
+// The process.argv() method is used for returning all the command-line arguments that were passed when the Node.js process was being launched
+// Command-line arguments are stored in constant variable hue & constant variable luminosity
+const hue = process.argv[2];
+const luminosity = process.argv[3];
 
-function getRandomColor() {
-  // find how to get random color and return it.
-  const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-  return randomColor;
-}
+// Function stored in constant variable named color
+// Returns a hex code for a light blue etc.
+const color = randomColor({
+  luminosity: luminosity,
+  hue: hue,
+});
 
-const randomSavedColor = getRandomColor();
+// chalk.hex which defines my own theme is called on color that is stored in a constant variable named randomColorChalk
+const randomColorChalk = chalk.hex(color);
 
-const randomColorChalk = chalk.hex(randomSavedColor);
-
+// randomColorChalk is called by logging it to the console with the # characters
+// color is called by using module literal
 console.log(randomColorChalk('###############################'));
 console.log(randomColorChalk('###############################'));
 console.log(randomColorChalk('###############################'));
 console.log(randomColorChalk('#####                     #####'));
-console.log(randomColorChalk(`#####       ${randomSavedColor}       #####`));
+console.log(randomColorChalk(`#####       ${color}       #####`));
 console.log(randomColorChalk(`#####                     #####`));
 console.log(randomColorChalk('###############################'));
 console.log(randomColorChalk('###############################'));
 console.log(randomColorChalk('###############################'));
+
+// When user enters node index.js it generates a 9x31 '#' characters long block as the random color that was generated and the hex code of the random number is printed in the middle of the block
+
+// // A function with empty parameter
+// function getRandomColor() {
+//   // find how to get random color and return it.
+//   const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+//   return randomColor;
+// }
+
+// // Function getRandomColor() is called and assigned in a constant variable called randomSavedColor
+// const randomSavedColor = getRandomColor();
 
 // function isPrime(num) {
 //   if (num < 2 || !Number.isInteger(num)) return false;
